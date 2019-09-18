a3d4a8d38c21a6a83b52325bdb32d7feaa4424a4
Jason Clark <jason@jjasonclark.com>
2019-09-18 13:25:53 -0700

    formatting

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,25 +1,25 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Web.Script.Serialization;
 
 namespace analytics_gateway.Controllers
 {
-  [Route("/")]
-  [ApiController]
-  public class ValuesController : ControllerBase
-  {
-    // GET /
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
     {
+        // GET /
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
             var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
             var query = "EVALUATE Grants";
-            var result = Query.Sample(connectionString, query);
-            result.Wait();
-            var finalResult = new List<string>();
-            result.Result.ForEach(row => finalResult.Add(new JavaScriptSerializer().Serialize(row)));
-            return finalResult;
+            var queryTask = Query.Sample(connectionString, query);
+            queryTask.Wait();
+            var jsonResult = new List<string>();
+            queryTask.Result.ForEach(row => jsonResult.Add(new JavaScriptSerializer().Serialize(row)));
+            return jsonResult;
         }
     }
 }
